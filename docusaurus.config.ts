a2a175637805f6b6c9e7cb36f595fe8d300e623a
Greg T. Wallace <greg@gregtwallace.com>
2024-03-25 19:21:23 -0400

    add donate options

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
@@ -73,6 +73,12 @@ const config: Config = {
           label: 'Forum',
           position: 'left',
         },
+        {
+          to: 'docs/donate',
+          label: 'Donate',
+          position: 'left',
+          activeBaseRegex: '$^', // never show active
+        },
       ],
     },
     footer: {
@@ -108,6 +114,10 @@ const config: Config = {
         {
           title: 'More',
           items: [
+            {
+              label: 'Donate',
+              to: 'docs/donate',
+            },
             {
               label: 'Source Code',
               href: 'https://github.com/gregtwallace/legocerthub',
